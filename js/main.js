a1d74c9566b40116aa90d486fe8418587dbc5a88
micaparat <micaparat@gmail.com>
2024-02-06 21:01:30 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,389 @@
+let nume = [
+  "Eugen",
+  "Belzebut",
+  "Duţulache",
+  "Og",
+  "Ramses",
+  "Ludovic",
+  "Mihail",
+  "Nicuşor",
+  "Marieta",
+  "Ana",
+  "Darius",
+  "Gerula",
+  "Spartacus",
+  "Nelu",
+  "Leon",
+  "Brunhilda",
+  "Solomon",
+  "Florinela",
+  "Marcel",
+  "Ştefănuţ",
+  "Dumitru",
+  "Zamfira",
+  "Fram",
+  "Mickey",
+  "Sabrina",
+  "Gigi",
+  "Macaulay",
+  "Miron",
+  "Satoshi",
+  "Ieremia",
+  "Eudochia",
+  "Ionela",
+  "Elon",
+  "Jeaneta",
+  "Carmen",
+  "Aureola",
+  "Calistrat",
+  "Duiliu",
+  "Arsenie",
+  "Geani",
+  "Timotei",
+  "Dalai",
+  "Benone",
+  "Tamara",
+];
+
+let ocupatii = [
+  "un puşti",
+  "un demon",
+  "un brontozaur",
+  "un neanderthalian",
+  "un faraon",
+  "un principe",
+  "un arhanghel",
+  "un primar",
+  "o fată",
+  "o tipă",
+  "un mititel",
+  "un dac",
+  "un gladiator",
+  "un domn",
+  "un revoluţionar",
+  "o vikingă",
+  "un prichindel",
+  "o copiliţă",
+  "un ţânc",
+  "un pezevenghi",
+  "un plod",
+  "o cuconiţă",
+  "un urs polar",
+  "un mouse",
+  "o vrăjitoare",
+  "un cioban",
+  "un actor",
+  "un vlăjgan",
+  "un samurai",
+  "un voievod",
+  "o tanti",
+  "o persoană",
+  "un magnat",
+  "o fetiţă",
+  "o puştoaică",
+  "o copilă",
+  "un holtei",
+  "un june",
+  "un băieţel",
+  "un băieţaş",
+  "un şampon",
+  "un lama tibetan",
+  "un interpret",
+  "o mătuşă",
+];
+
+let anotimpuri = [
+  "iarnă",
+  "Cretacic",
+  "primăvară",
+  "Jurasic",
+  "vară",
+  "toamnă",
+  "ianuarie",
+  "februarie",
+  "an bisect",
+  "Ev Mediu",
+  "noiembrie",
+  "decembrie",
+  "Vărsător",
+  "Mercur retrograd",
+  "pandemie",
+  "septembrie",
+  "post-apocalipsă",
+  "mai",
+  "august",
+  "1944",
+  "Halloween",
+  "Bobotează",
+  "9000 î. e. n.",
+];
+
+let orase = [
+  "Miercurea Nirajului",
+  "Popeşti",
+  "Berceni",
+  "Rahova",
+  "Pantelimon",
+  "Titan",
+  "Grojdibodu",
+  "Fierbinţi",
+  "Cluj",
+  "Oradea",
+  "Constanţa",
+  "Iaşi",
+  "Suceava",
+  "Giurgiu",
+  "Adunaţii Copăceni",
+  "Scorniceşti",
+  "Voluntari",
+  "Craiova",
+  "Timişoara",
+  "Tecuci",
+  "Băile Tuşnad",
+  "Vârteşcoiu",
+  "Băicoi",
+  "Gurbăneşti",
+  "Adjud",
+  "Râmnicu Sărat",
+  "Lugoj",
+  "Deva",
+  "Zalău",
+  "Vama Veche",
+  "Saturn",
+  "Sovata",
+  "Fundulea",
+  "Zărneşti",
+  "Braşov",
+  "Galaţi",
+  "Brăila",
+  "Botoşani",
+  "Dorohoi",
+  "Fundu Moldovei",
+  "Măciuca",
+  "Mierleştii de Sus",
+  "Udaţi-Mânzu",
+  "Medgidia",
+  "Beiuş",
+  "Teiuş",
+  "Curtici",
+  "Şimleu Silvaniei",
+  "Dolhasca",
+  "Paşcani",
+  "Caracal",
+  "Târgu Jiu",
+  "Târgu Mureş",
+  "Curtea de Argeş",
+];
+
+let rudenii = [
+  "mama",
+  "tatăl",
+  "bunica",
+  "bunicul",
+  "geamănul malefic al",
+  "străbunica",
+  "străbunicul",
+  "strămoşul",
+  "învăţătoarea",
+  "unchiul",
+  "cuscrul",
+  "mătuşa",
+  "amanta tatălui",
+  "şogorul vecinului",
+  "îngerul păzitor al",
+  "mama vitregă a",
+  "tataia",
+  "mamaia",
+  "babacul",
+  "născătoarea",
+];
+
+let probleme = [
+  "i-a căzut telefonul în butoiul cu must",
+  "i-a înghiţit coşul de la supermarket o monedă ediţie limitată",
+  "ceilalţi copii au luat o notă mai mare la Educaţie Fizică",
+  "nimeni nu se mai uită la MASH în zilele noastre",
+  "cineva i-a dărâmat îngheţata de pe cornet",
+  "persoane necunoscute au scris ceva urât despre Rapid pe un gard",
+  "pe o maşină undeva în cartier scrie SPALĂ-MĂ",
+  "un pescăruş i-a ciugulit cartofii proaspăt cumpăraţi de la Mec",
+  "nişte golani au fumat marihuana şi au cântat la chitară în parc",
+  "mama lui Ştefan cel Mare l-a alungat pe acesta de la poartă",
+  "Milli Vanilli au făcut playback în anii 80",
+  "cineva i-a furat banii de sendviş din offshore-ul din insulele Cayman",
+  "cineva i-a suflat un cornet în păr",
+  "i s-a lipit o gumă de mestecat de blugi",
+  "Zâna Măseluţă i-a dat ţeapă cu 4 lei",
+  "şi-a pierdut ultimul iPhone în reprezentanţa Louis Vuitton",
+  "i s-a băgat cineva în selfie",
+];
+
+let rezolvari = [];
+
+let fantani = [
+  "un aspersor",
+  "un robinet",
+  "un furtun de pompieri",
+  "cascada Niagara",
+  "o ţâşnitoare",
+  "o cişmea",
+  "Canalul Dunăre-Marea Neagră",
+  "Oltul şi Mureşul la un loc",
+  "un bidon cu dopul găurit",
+  "o ţeavă spartă",
+  "un gheţar afectat de încălzirea globală",
+  "un pistol cu apă",
+  "o icoană miraculoasă",
+];
+
+let prajituri = [
+  "tort",
+  "chec",
+  "cozonac",
+  "panettone",
+  "lava cake",
+  "brioşă",
+  "fursec",
+  "amandină",
+  "banană",
+  "bomboană",
+  "teanc de bani",
+  "homar",
+  "ghindă",
+  "castravete",
+  "plăcintă",
+  "pateu",
+  "cornuleţ cu lapte",
+  "drob",
+  "porc",
+  "parizer",
+  "şalău",
+  "caşcaval pane",
+  "turtă dulce",
+  "savarină",
+  "broccoli",
+  "cernoziom",
+  "pâine cu unt",
+  "pizza",
+];
+
+let sarbatori = [];
+
+let felicitari = [
+  "Hip, hip, ura!",
+  "Bv tată, mari autorităţi!",
+  "DURA LEX SED LEX!",
+  "Aliluia!",
+  "Ura şi la gară!",
+  "Cangratulation’s every bode!",
+  "La mulţi ani 1997!",
+  "Felicitări!",
+  "Felikitări!",
+  "Trăiască!",
+  "Siguranţă şi încredere!",
+  "Cine-i născut în ianuarie, hai sus!",
+  "Aşa da, şi în nici un caz altfel!",
+];
+
+function getRandomIndex(array) {
+  const index = Math.floor(Math.random() * array.length);
+  return index;
+}
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function handleClick() {
+  //nume
+  let numeText = document.getElementById("nume");
+  let nume2Text = document.getElementById("nume2");
+  let nume3Text = document.getElementById("nume3");
+  let randomNumeIndex = getRandomIndex(nume);
+  numeText.innerText = nume[randomNumeIndex];
+  nume2Text.innerText = nume[randomNumeIndex];
+  nume3Text.innerText = nume[randomNumeIndex];
+
+  //ocupatie
+  let ocupatieText = document.getElementById("ocupatie");
+  let ocupatie2Text = document.getElementById("ocupatie2");
+  ocupatieText.innerText = ocupatii[randomNumeIndex];
+  ocupatie2Text.innerText = ocupatii[randomNumeIndex];
+
+  //rudenie
+  let rudenieText = document.getElementById("rudenie");
+  let randomRudenieIndex = getRandomIndex(rudenii);
+  rudenieText.innerText = rudenii[randomRudenieIndex];
+
+  //varsta
+  let varstaText = document.getElementById("varsta");
+  let varsta2Text = document.getElementById("varsta2");
+  let randomVarsta = randomIntFromInterval(2, 18);
+  varstaText.innerText = randomVarsta;
+  varsta2Text.innerText = randomVarsta;
+
+  //probleme
+  let problemaText = document.getElementById("problema");
+  let randomProblemaIndex = getRandomIndex(probleme);
+  problemaText.innerText = probleme[randomProblemaIndex];
+
+  //fantani
+  let fantanaText = document.getElementById("fantana");
+  let randomFantanaIndex = getRandomIndex(fantani);
+  fantanaText.innerText = fantani[randomFantanaIndex];
+
+  //rezolvari
+  let rezolvareText = document.getElementById("rezolvare");
+  let randomRezolvareIndex = getRandomIndex(rezolvari);
+  rezolvareText.innerText = rezolvari[randomRezolvareIndex];
+
+  //ore
+  let oreText = document.getElementById("timp");
+  let randomOra = randomIntFromInterval(2, 72);
+  oreText.innerText = randomOra;
+
+  //prajituri
+  let prajituraText = document.getElementById("praji");
+  let randomPrajiIndex = getRandomIndex(prajituri);
+  prajituraText.innerText = prajituri[randomPrajiIndex];
+
+  //sarbatori
+  let sarbatoareText = document.getElementById("sarbatoare");
+  let randomSarbatoareIndex = getRandomIndex(sarbatori);
+  sarbatoareText.innerText = sarbatori[randomSarbatoareIndex];
+
+  //anotimpuri
+  let anotimpText = document.getElementById("anotimp");
+  let randomAnotimpIndex = getRandomIndex(anotimpuri);
+  anotimpText.innerText = anotimpuri[randomAnotimpIndex];
+
+  //orase
+  let orasText = document.getElementById("oras");
+  let oras2Text = document.getElementById("oras2");
+  let randomOrasIndex = getRandomIndex(orase);
+  orasText.innerText = orase[randomOrasIndex];
+  oras2Text.innerText = orase[randomOrasIndex];
+
+  //felicitari
+  let felicitariText = document.getElementById("felicitari");
+  let randomFelicitareIndex = getRandomIndex(felicitari);
+  felicitariText.innerText = felicitari[randomFelicitareIndex];
+
+  //comments
+  let commentsText = document.getElementById("num-comments");
+  let randomNumComments = randomIntFromInterval(147, 11648);
+  commentsText.innerText = randomNumComments;
+}
+
+function likeButton() {
+  let graylike = document.querySelector(".like");
+  let likes = document.querySelector(".likes-span");
+  if (graylike.src.match("images/gray_like.png")) {
+    graylike.src = "images/blue_like.png";
+    likes.innerHTML = "500";
+  } else {
+    graylike.src = "images/gray_like.png";
+    likes.innerHTML = "499";
+  }
+}
