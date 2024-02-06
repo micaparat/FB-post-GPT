cc3c25cf8c1d0aade28bff611f00ce2a8191a2eb
micaparat <micaparat@gmail.com>
2024-02-06 22:02:43 +0200

    varsion 1.0

--- js/main.js
+++ js/main.js
@@ -173,6 +173,24 @@ let orase = [
   "Târgu Jiu",
   "Târgu Mureş",
   "Curtea de Argeş",
+  "Bran",
+  "Slănic Prahova",
+  "New Orleans",
+  "Salonta",
+  "Bronx",
+  "Jakarta",
+  "Comarnic",
+  "Primăverii",
+  "Colentina, inima străzii",
+  "Buzău",
+  "Focşani",
+  "Oneşti",
+  "Bacău",
+  "Vaslui",
+  "Huşi",
+  "Bârlad",
+  "Kathmandu",
+  "Trebuci",
 ];
 
 let rudenii = [
@@ -199,26 +217,76 @@ let rudenii = [
 ];
 
 let probleme = [
-  "i-a căzut telefonul în butoiul cu must",
-  "i-a înghiţit coşul de la supermarket o monedă ediţie limitată",
+  "copilului i-a căzut telefonul în butoiul cu must",
+  "le-a înghiţit coşul de la supermarket o monedă ediţie limitată",
   "ceilalţi copii au luat o notă mai mare la Educaţie Fizică",
   "nimeni nu se mai uită la MASH în zilele noastre",
-  "cineva i-a dărâmat îngheţata de pe cornet",
-  "persoane necunoscute au scris ceva urât despre Rapid pe un gard",
+  "cineva i-a dărâmat copilului îngheţata de pe cornet",
+  "vandalii au desenat o mustaţă pe afişul preferat cu Bănică al copilului",
+  "un pensionar şi-a agitat bastonul în direcţia lor",
+  "aşa-zisul Moş Crăciun era de fapt un tânăr cu barbă falsă",
+  "clanurile interlope au pus stăpânire asupra oraşului",
+  "asupra unor politicieni planează suspiciuni de corupţie",
+  "persoane necunoscute au mâzgălit ceva urât despre Rapid pe un gard",
   "pe o maşină undeva în cartier scrie SPALĂ-MĂ",
-  "un pescăruş i-a ciugulit cartofii proaspăt cumpăraţi de la Mec",
-  "nişte golani au fumat marihuana şi au cântat la chitară în parc",
+  "un pescăruş rău le-a ciugulit cartofii proaspăt cumpăraţi de la Mec",
+  "nişte derbedei au fumat marihuana şi au cântat la chitară în parc",
+  "vecinii au dat un party cu trap şi manele ignorând orele de linişte",
+  "un vecin le-a tăiat mingea copiilor",
+  "raţele din parc au refuzat salata pentru că s-au învăţat cu pâine",
+  "inflaţia galopantă a scumpit covrigul cu încă 50 de bani",
+  "China a renunţat la politica copilului unic",
+  "cineva dă cu bormaşina de jumătate de oră",
+  "li s-a pus pastă de dinţi Aquafresh pe clanţă",
   "mama lui Ştefan cel Mare l-a alungat pe acesta de la poartă",
   "Milli Vanilli au făcut playback în anii 80",
-  "cineva i-a furat banii de sendviş din offshore-ul din insulele Cayman",
-  "cineva i-a suflat un cornet în păr",
-  "i s-a lipit o gumă de mestecat de blugi",
+  "cineva le-a deturnat banii de sendviş din offshore-ul din insulele Cayman",
+  "cineva i-a suflat copilului un cornet în păr",
+  "o lucrare de doctorat s-a dovedit a fi plagiată",
+  "copilului i s-a lipit o gumă de mestecat de blugi",
   "Zâna Măseluţă i-a dat ţeapă cu 4 lei",
-  "şi-a pierdut ultimul iPhone în reprezentanţa Louis Vuitton",
-  "i s-a băgat cineva în selfie",
+  "copilul şi-a pierdut iPhoneul în reprezentanţa Louis Vuitton",
+  "s-a băgat în selfie-ul lor o persoană cu nasul mare",
 ];
 
-let rezolvari = [];
+let rezolvari = [
+  "deschis dosar penal pentru contrafacere de tenişi",
+  "mâncat două cartoane de gogoşi de la Fiţoşica",
+  "comandat două şaorme şi returnat una fiindcă avea pătrunjel",
+  "compus un articol viral pentru pagina de Facebook",
+  "apărat patru penalty-uri şi au câştigat Cupa Poliţiei",
+  "trimis un agent pe Marte să amendeze robotul Curiosity",
+  "desenat portretul robot al suspectului cu carioci originale Herlitz",
+  "înăbuşit violent un protest paşnic în Piaţa Victoriei",
+  "destructurat o reţea de badminton care călca pe iarbă în Cişmigiu",
+  "confiscat o praştie artizanală şi 4 proiectile din pietriş lacustru",
+  "scris 10 glume bune pentru roastul unui coleg",
+  "dus maşina şefului la spălătoria cu fise de peste drum",
+  "interogat persoana bănuită că ar fi înfipt zambila",
+  "reconstituit traseul fiecărui pasager prins fără bilet în tramvai",
+  "triangulat poziţia galaxiei Andromeda",
+  "urmărit cu sufletul la gură un sezon întreg din Star Trek: TNG",
+  "dezlegat volumul 4 al revistei Integrame Pentru Începători",
+  "sunat la teleshopping să comande revoluţionarul Happy Chop",
+  "învăţat Luceafărul pe de rost",
+  "luat bătaie la table de la un băiat pe nume Briliantu",
+  "făcut figuraţie într-o reclamă la alifie pentru amigdale",
+  "învăţat limba klingonă pe Duolingo",
+  "alcătuit un eseu despre drama intelectualului în romanul interbelic",
+  "dat vaca la schimb pe un pumn de fasole, chipurile, magică",
+  "ajuns la casa bunicii cu un coş cu merinde",
+  "biruit asupra zmeului, cu toate că prinţesa era de fapt în alt castel",
+  "terminat campania protoşilor din Starcraft fără coduri",
+  "inventat un aparat de zbor care funcţionează doar cu apă",
+  "buşit o dronă încercând să filmeze într-o peşteră în Grecia",
+  "cerut măriri de salariu",
+  "absolvit Medicina şi s-au specializat în neurochirurgie",
+  "ieşit la pensie",
+  "făcut un grătar de zile mari",
+  "construit o şură trainică",
+  "organizat un flashmob",
+  "dat pisicuţa jos din pom",
+];
 
 let fantani = [
   "un aspersor",
@@ -267,7 +335,41 @@ let prajituri = [
   "pizza",
 ];
 
-let sarbatori = [];
+let sarbatori = [
+  "pomana porcului",
+  "o postare cu 7000 de likeuri",
+  "un selfie",
+  "baloane de săpun",
+  "un târg de Crăciun în Piaţa Constituţiei",
+  "o biserică",
+  "un ritual satanic",
+  "o vrajă puternică",
+  "un chef cu manele",
+  "o expediţie la Polul Sud",
+  "dovlecei pane cu tzatziki",
+  "o horă",
+  "un festival de folk",
+  "un talk-show politic",
+  "un pas înainte spre comunism",
+  "compot",
+  "un grătar în livadă",
+  "ţuică din toate corcoduşele culese pe drum",
+  "schimb de id-uri de Yahoo Messenger",
+  "cercuri în nisip pentru a-şi dovedi teorema",
+  "o serată dansantă",
+  "o plimbare pe faleză",
+  "plajă până s-au cojit pe nas",
+  "o reţetă văzută la Jamila",
+  "un scurtmetraj indie în limba daneză",
+  "un dulap de la IKEA",
+  "schimb de figurine cu ştrumfi",
+  "tema de vacanţă la mate",
+  "un album techno intitulat POLIZEIARBEIT",
+  "bancuri cu soacre",
+  "cafeluţă",
+  "content pentru canalul de tiktok",
+  "concurs de alergat în saci",
+];
 
 let felicitari = [
   "Hip, hip, ura!",
@@ -281,8 +383,52 @@ let felicitari = [
   "Felikitări!",
   "Trăiască!",
   "Siguranţă şi încredere!",
+  "Mazel tov!",
+  "Valar morghulis!",
+  "La mai mare!",
+  "Forza Steaua!",
+  "Big up!",
+  "Like, share şi subscribe!",
+  "Aşa ceva!",
+  "Nu ne mai facem bine!",
+  "Viaţa fără poezie e greu!",
+  "A fost o zi minunată!",
+  "Un Crăciun de pomină!",
+  "Ptiu, ptiu, să nu te deochi!",
+  "Cool cool cool!",
+  "Cowabunga!",
+  "La Vama Veche, în ureche!",
+  "Când ne-o fi mai rău, aşa să ne fie!",
+  "Şi-am încălecat pe-o şa!",
+  "Ecce homo!",
+  "Priveşte, mamă, fiul tău!",
+  "O, paşă, tu ce darnic eşti!",
+  "Şi au trăit fericiţi până la adânci bătrâneţi!",
   "Cine-i născut în ianuarie, hai sus!",
   "Aşa da, şi în nici un caz altfel!",
+];
+
+let bodyParts = [
+  "nas",
+  "frunte",
+  "ochi",
+  "urechi",
+  "ceafă",
+  "plexul solar",
+  "călcâie",
+  "spate",
+  "scalp",
+  "sprâncene",
+  "încheieturi",
+  "dinţi",
+  "coate",
+  "pupile",
+  "plămâni",
+  "tendonul lui Ahile",
+  "obraji",
+  "pleoape",
+  "subraţ",
+  "buze",
 ];
 
 function getRandomIndex(array) {
@@ -297,83 +443,69 @@ function randomIntFromInterval(min, max) {
 
 function handleClick() {
   //nume
-  let numeText = document.getElementById("nume");
-  let nume2Text = document.getElementById("nume2");
-  let nume3Text = document.getElementById("nume3");
   let randomNumeIndex = getRandomIndex(nume);
-  numeText.innerText = nume[randomNumeIndex];
-  nume2Text.innerText = nume[randomNumeIndex];
-  nume3Text.innerText = nume[randomNumeIndex];
+  updateTextContent("nume", nume[randomNumeIndex]);
 
   //ocupatie
-  let ocupatieText = document.getElementById("ocupatie");
-  let ocupatie2Text = document.getElementById("ocupatie2");
-  ocupatieText.innerText = ocupatii[randomNumeIndex];
-  ocupatie2Text.innerText = ocupatii[randomNumeIndex];
+  updateTextContent("ocupatie", ocupatii[randomNumeIndex]);
 
   //rudenie
-  let rudenieText = document.getElementById("rudenie");
   let randomRudenieIndex = getRandomIndex(rudenii);
-  rudenieText.innerText = rudenii[randomRudenieIndex];
+  updateTextContent("rudenie", rudenii[randomRudenieIndex]);
 
   //varsta
-  let varstaText = document.getElementById("varsta");
-  let varsta2Text = document.getElementById("varsta2");
   let randomVarsta = randomIntFromInterval(2, 18);
-  varstaText.innerText = randomVarsta;
-  varsta2Text.innerText = randomVarsta;
+  updateTextContent("varsta", randomVarsta);
 
   //probleme
-  let problemaText = document.getElementById("problema");
   let randomProblemaIndex = getRandomIndex(probleme);
-  problemaText.innerText = probleme[randomProblemaIndex];
+  updateTextContent("problema", probleme[randomProblemaIndex]);
 
   //fantani
-  let fantanaText = document.getElementById("fantana");
   let randomFantanaIndex = getRandomIndex(fantani);
-  fantanaText.innerText = fantani[randomFantanaIndex];
+  updateTextContent("fantana", fantani[randomFantanaIndex]);
 
   //rezolvari
-  let rezolvareText = document.getElementById("rezolvare");
   let randomRezolvareIndex = getRandomIndex(rezolvari);
-  rezolvareText.innerText = rezolvari[randomRezolvareIndex];
+  updateTextContent("rezolvare", rezolvari[randomRezolvareIndex]);
 
   //ore
-  let oreText = document.getElementById("timp");
   let randomOra = randomIntFromInterval(2, 72);
-  oreText.innerText = randomOra;
+  updateTextContent("timp", randomOra);
 
   //prajituri
-  let prajituraText = document.getElementById("praji");
   let randomPrajiIndex = getRandomIndex(prajituri);
-  prajituraText.innerText = prajituri[randomPrajiIndex];
+  updateTextContent("praji", prajituri[randomPrajiIndex]);
 
   //sarbatori
-  let sarbatoareText = document.getElementById("sarbatoare");
   let randomSarbatoareIndex = getRandomIndex(sarbatori);
-  sarbatoareText.innerText = sarbatori[randomSarbatoareIndex];
+  updateTextContent("sarbatoare", sarbatori[randomSarbatoareIndex]);
 
   //anotimpuri
-  let anotimpText = document.getElementById("anotimp");
   let randomAnotimpIndex = getRandomIndex(anotimpuri);
-  anotimpText.innerText = anotimpuri[randomAnotimpIndex];
+  updateTextContent("anotimp", anotimpuri[randomAnotimpIndex]);
 
   //orase
-  let orasText = document.getElementById("oras");
-  let oras2Text = document.getElementById("oras2");
   let randomOrasIndex = getRandomIndex(orase);
-  orasText.innerText = orase[randomOrasIndex];
-  oras2Text.innerText = orase[randomOrasIndex];
+  updateTextContent("oras", orase[randomOrasIndex]);
+
+  //parte a corpului
+  let randomBodyIndex = getRandomIndex(bodyParts);
+  updateTextContent("partea-corpului", bodyParts[randomBodyIndex]);
 
   //felicitari
-  let felicitariText = document.getElementById("felicitari");
   let randomFelicitareIndex = getRandomIndex(felicitari);
-  felicitariText.innerText = felicitari[randomFelicitareIndex];
+  updateTextContent("felicitari", felicitari[randomFelicitareIndex]);
 
   //comments
-  let commentsText = document.getElementById("num-comments");
-  let randomNumComments = randomIntFromInterval(147, 11648);
-  commentsText.innerText = randomNumComments;
+  updateTextContent("num-comments", randomIntFromInterval(147, 11648));
+}
+
+function updateTextContent(dataTarget, value) {
+  const elements = document.querySelectorAll(`[data-target="${dataTarget}"]`);
+  elements.forEach((element) => {
+    element.innerText = value;
+  });
 }
 
 function likeButton() {
