7813a427aeed1820e2b1b851de324912f88037b9
utopicbalcanic <130102118+utopicbalcanic@users.noreply.github.com>
2024-02-07 09:46:50 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -643,8 +643,6 @@ function handleClick() {
   //comments
   updateTextContent("num-comments", randomIntFromInterval(147, 11648));
 
-  //laics
-  updateTextContent("num-laics", randomIntFromInterval(147, 948));
 }
 
 function updateTextContent(dataTarget, value) {
